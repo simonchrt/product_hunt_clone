84712ec23fb8656eb83e807b3424dfdfc89ed553
Simon Chretien <simon.chretien62@gmail.com>
2022-06-03 10:01:49 +0200

    Add github integration

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,4 +4,24 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(params_product)
+    if @product.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def params_product
+    params.require(:product).permit(:name, :tagline)
+  end
+
 end
